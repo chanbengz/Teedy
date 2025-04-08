522ba5a8a7be5095fe5e4c5572e560d1d16d6dd4
chanbengz <chanben04gz@gmail.com>
2025-04-08 02:36:14 +0000

    test: add jacoco report

--- docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
+++ docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
@@ -7,6 +7,12 @@ import com.sismics.docs.core.util.TransactionUtil;
 import com.sismics.docs.core.util.authentication.InternalAuthenticationHandler;
 import org.junit.Assert;
 import org.junit.Test;
+import com.sismics.docs.core.dao.GroupDao;
+import com.sismics.docs.core.dao.criteria.GroupCriteria;
+import com.sismics.docs.core.dao.dto.GroupDto;
+import com.sismics.docs.core.model.jpa.Group;
+import com.sismics.docs.core.model.jpa.UserGroup;
+import java.util.List;
 
 /**
  * Tests the persistance layer.
@@ -32,6 +38,72 @@ public class TestJpa extends BaseTransactionalTest {
 
         // Delete the created user
         userDao.delete("testJpa", user.getId());
+        TransactionUtil.commit();
+    }
+
+    @Test
+    public void testJpa2() throws Exception {
+        UserDao userDao = new UserDao();
+        User user = createUser("groupTest");
+        TransactionUtil.commit();
+
+        GroupDao groupDao = new GroupDao();
+        Group group = new Group();
+        group.setName("testGroup");
+        String groupId = groupDao.create(group, user.getId());
+        TransactionUtil.commit();
+
+        // Get the group by ID
+        Group groupById = groupDao.getActiveById(groupId);
+        Assert.assertNotNull(groupById);
+        Assert.assertEquals("testGroup", groupById.getName());
+
+        Group groupByName = groupDao.getActiveByName("testGroup");
+        Assert.assertNotNull(groupByName);
+        Assert.assertEquals(groupId, groupByName.getId());
+
+        Group childGroup = new Group();
+        childGroup.setName("childGroup");
+        childGroup.setParentId(groupId);
+        String childGroupId = groupDao.create(childGroup, user.getId());
+        TransactionUtil.commit();
+
+        group.setName("updatedGroup");
+        groupDao.update(group, user.getId());
+        TransactionUtil.commit();
+
+        Group updatedGroup = groupDao.getActiveById(groupId);
+        Assert.assertEquals("updatedGroup", updatedGroup.getName());
+
+        UserGroup userGroup = new UserGroup();
+        userGroup.setGroupId(groupId);
+        userGroup.setUserId(user.getId());
+        String userGroupId = groupDao.addMember(userGroup);
+        TransactionUtil.commit();
+
+        GroupCriteria criteria = new GroupCriteria();
+        criteria.setSearch("updated");
+        List<GroupDto> groups = groupDao.findByCriteria(criteria, null);
+        Assert.assertFalse(groups.isEmpty());
+        Assert.assertEquals(groupId, groups.get(0).getId());
+
+        criteria = new GroupCriteria();
+        criteria.setUserId(user.getId());
+        criteria.setRecursive(true);
+        groups = groupDao.findByCriteria(criteria, null);
+        Assert.assertFalse(groups.isEmpty());
+
+        groupDao.removeMember(groupId, user.getId());
+        TransactionUtil.commit();
+
+        groupDao.delete(childGroupId, user.getId());
+        groupDao.delete(groupId, user.getId());
+        TransactionUtil.commit();
+
+        Assert.assertNull(groupDao.getActiveById(groupId));
+        Assert.assertNull(groupDao.getActiveById(childGroupId));
+
+        userDao.delete("groupTest", user.getId());
         TransactionUtil.commit();
     }
 }
